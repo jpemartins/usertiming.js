f7ae6df56df6bb40ef5fd877f7a2d045108b11ec
Joao Martins <joao.marcal.martins@gmail.com>
2013-12-26 16:58:37 +0000

    Updated to the latest changes of the API.
    * Chrome ships already this API so this for the
    other browsers.
    
    Changes to the API:
    * There is no getMarks neither getMeasures
    * Use getEntriesByName or getEntriesByType(type) being
    type: "mark" or "measure".
    
    * Tests still missing.

--- usertiming.js
+++ usertiming.js
@@ -1,15 +1,34 @@
 (function () {
-	var navStart = performance.timing.navigationStart
-	  , fetchStart = performance.timing.fetchStart;
+	if (window.performance["mozMark"] || window.performance["mark"]) {
+		return;
+	}
+
+	function PerformanceEntry(name, entryType, startTime, duration) {
+		this.name = name;
+		this.entryType = entryType;
+		this.startTime = startTime;
+		this.duration = duration;
+	}
+
+	function PerformanceMeasure(name, startTime, duration) {
+		PerformanceEntry.apply(this, [name, "measure", startTime, duration ]);
+	}
+	PerformanceMeasure.prototype = PerformanceEntry.prototype;
+
+	function PerformanceMark(name, startTime) {
+		PerformanceEntry.apply(this, [name, "mark", startTime, 0 ]);
+	}
+	PerformanceMeasure.prototype = PerformanceEntry.prototype;
+
+	var navStart = performance.timing.navigationStart;
 
 	/* DOMHighResTimeStamp is the relative time to navigationStart
 	 * The precision of the timer is down to a tenth of a milisecond (double value)
      */
-	var tick = !!window["DOMHighResTimeStamp"] ? performance.now : (function () {
-		return Date.now() - navStart;
-	});
+	var tick = !!performance.now ? (function(){ return performance.now() }) : 
+			(function () { return Date.now() - navStart; });
 
-	var marks = {}, measures = {};
+	const marks = {}, measures = {};
 
 	var entries = {
 		'marks': marks
@@ -21,15 +40,12 @@
 			throw new Error("mark name not allowed");
 		}
 
-		(marks[name] = marks[name] || []).push(tick());
-	}
-
-	performance.__proto__.getMarks = function (name) {
-		return marks[name] || marks;
+		(marks[name] = marks[name] || []).push(new PerformanceMark(name, tick()));
 	}
 
 	performance.__proto__.clearMarks = function (name) {
 		if (!name) {
+			delete marks;
 			return (marks = {});
 		}
 		
@@ -37,35 +53,57 @@
 	}
 
 	performance.__proto__.measure = function (name, startMark, endMark) {
+		var duration, startTime, startIdx, endIdx;
+		var startMarks = marks[startMark||""]
+		  , endMarks = marks[endMark||""];
+
 		if (!measures[name]) {
 			measures[name] = [];	
 		}
 
-		if (!marks[startMark]) {
-			measures[name].push(Date.now() - fetchStart);	
-			return;
+		startIdx = (startMarks || []).length - 1;
+
+		if (!endMarks && startMarks) {
+			startTime = startMarks[startIdx].startTime;
+			duration = startTime - performance.now();
+		} else if (!startMarks) {
+			startTime = performance.now();
+			duration = startTime - navStart;
+		} else if (endMarks && startMarks) {
+			endIdx = (endMarks || []).length - 1;			
+			startTime = startMarks[startIdx].startTime;
+			duration = endMarks[endIdx].startTime - startTime
 		}
 
-		var startIdx, endIdx, evalue, length;
-
-		startIdx = (marks[startMark] || []).length - 1;
-		endIdx = (marks[endMark] || []).length - 1;
-
-		evalue = marks[endMark] !== undefined ? marks[endMark][endIdx] : fetchStart;
-
-		measures[name].push(marks[endMark][endIdx] - marks[startMark][startIdx]);
-	}
-
-	performance.__proto__.getMeasures = function (name) {
-		return measures[name] || measures;
+		measures[name].push(new PerformanceMeasure(name, 
+			startTime, duration));
 	}
 
 	performance.__proto__.clearMeasures = function (name) {
 		if (!name) {
+			delete measures;
 			return (measures = {});
 		}
 		
 		measures[name] = [];
 	}
+
+	performance.__proto__.getEntriesByName = function (name) {
+		return measures[name] || measures;
+	}
+
+	performance.__proto__.getEntriesByType = function (type) {
+		var arr;
+		if (type == "measures") {
+			arr = measures;
+		} else if (type == "mark") {
+			arr = []
+			for (p in marks) {
+				arr = arr.concat(marks[p]);
+			}
+		}
+		return arr;
+	}
+
 }());
 
